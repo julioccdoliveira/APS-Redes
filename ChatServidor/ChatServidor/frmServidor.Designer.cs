0640725941d86d69ef4e3b31d38b18eea5bb5c6d
julioccdoliveira <51184483+julioccdoliveira@users.noreply.github.com>
2019-11-12 18:15:57 -0300

    Add files via upload

---
+++ ChatServidor/ChatServidor/frmServidor.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace ChatServidor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.btnAtender = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(13, 84);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ReadOnly = true;
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLog.Size = new System.Drawing.Size(390, 229);
+            this.txtLog.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Endereço IP : ";
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(94, 30);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(179, 20);
+            this.txtIP.TabIndex = 2;
+            this.txtIP.Text = "127.0.0.1";
+            // 
+            // btnAtender
+            // 
+            this.btnAtender.Location = new System.Drawing.Point(279, 27);
+            this.btnAtender.Name = "btnAtender";
+            this.btnAtender.Size = new System.Drawing.Size(124, 23);
+            this.btnAtender.TabIndex = 3;
+            this.btnAtender.Text = "Iniciar Atendimento";
+            this.btnAtender.UseVisualStyleBackColor = true;
+            this.btnAtender.Click += new System.EventHandler(this.btnAtender_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Porta: ";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(94, 56);
+            this.txtPort.MaxLength = 6;
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(179, 20);
+            this.txtPort.TabIndex = 5;
+            this.txtPort.Text = "21010";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.NavajoWhite;
+            this.ClientSize = new System.Drawing.Size(418, 407);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnAtender);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtLog);
+            this.Name = "Form1";
+            this.Text = "Chat - Servidor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.Button btnAtender;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPort;
+    }
+}
+
---
+++ ChatServidor/ChatServidor/frmServidor.cs
@@ -0,0 +1,76 @@
+﻿using System.Windows.Forms;
+using System.Net;
+using System;
+
+namespace ChatServidor
+{
+    public partial class Form1 : Form
+    {
+        private delegate void AtualizaStatusCallback(string strMensagem);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAtender_Click(object sender, System.EventArgs e)
+        {
+            if (txtIP.Text==string.Empty)
+            {
+                MessageBox.Show("Informe o endereço IP.");
+                txtIP.Focus();
+                return;
+            }
+            
+            //Verifica se a porta digitada está em formato válido, se não estiver, coloca no valor padrão 21010.
+            if (int.TryParse(txtPort.Text, out int boolporta) == false)
+            {
+                MessageBox.Show("A porta informada está com valor inválido. O valor padrão (21010) será atribuido.");
+                txtPort.Text = "21010";
+            }
+            if (int.Parse(txtPort.Text) > 65335 || int.Parse(txtPort.Text) <= 0)
+            {
+                MessageBox.Show("A porta selecionada está fora dos alcance de portas TCP. O programa utilizará o valor padrão (21010).");
+                txtPort.Text = "21010";
+            }
+            try
+            {
+                // Analisa o endereço IP do servidor informado no textbox
+                IPAddress enderecoIP = IPAddress.Parse(txtIP.Text);
+                int porta = int.Parse(txtPort.Text);
+                // Cria uma nova instância do objeto ChatServidor
+                ChatServidor mainServidor = new ChatServidor(enderecoIP, porta);
+
+                // Vincula o tratamento de evento StatusChanged a mainServer_StatusChanged
+                ChatServidor.StatusChanged += new StatusChangedEventHandler(mainServidor_StatusChanged);
+
+
+
+                
+                // Inicia o atendimento das conexões
+                mainServidor.IniciaAtendimento();
+                txtLog.AppendText("Monitorando as conexões na porta " + porta + " ...\r\n");
+                txtPort.Enabled = false;
+                txtIP.Enabled = false;
+                btnAtender.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro de conexão: " + ex.Message);
+            }
+            
+        }
+
+        public void mainServidor_StatusChanged(object sender, StatusChangedEventArgs e)
+        {
+            // Chama o método que atualiza o formulário
+            this.Invoke(new AtualizaStatusCallback(this.AtualizaStatus), new object[] { e.EventMessage });
+        }
+
+        private void AtualizaStatus(string strMensagem)
+        {
+            // Atualiza o logo com mensagens
+            txtLog.AppendText(strMensagem + "\r\n");
+        }
+    }
+}
